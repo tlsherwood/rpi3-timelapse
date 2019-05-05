8f0df0603f46616a0998883c7c2a24e4ba3a6d34
tlsherwood <toddsherwood@outlook.com>
2019-05-05 19:23:01 -0400

    minor tweaks to variable naming and some input values based on trial run

--- timelapse.py
+++ timelapse.py
@@ -1,16 +1,16 @@
 from time import sleep
-import datetime
+# import datetime
 import picamera
-from picamera import PiCamera as camera
+# from picamera import PiCamera as camera
 
-"""video_length_seconds is the length of video desired (in seconds) assuming fps settings"""
-video_length_seconds = 30
-fps = 30
+"""VIDEO_LENGTH is the length of video desired (in seconds) assuming frame per second (FPS) settings"""
+VIDEO_LENGTH = 60
+FPS = 15
 
 """the remaining code can be left as is unless something different is desired"""
-"""WAIT_TIME is the amount of time between photos"""
-WAIT_TIME = 10
-number_of_pictures = fps * video_length_seconds
+"""WAIT_TIME is the amount of time between photos (in seconds)"""
+WAIT_TIME = 2.5
+number_of_pictures = FPS * VIDEO_LENGTH
 
 with picamera.PiCamera() as camera:
     try:
